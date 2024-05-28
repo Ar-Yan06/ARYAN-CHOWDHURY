72b86c44c39b1aa74c1310cb447c5e59481c414c
RAKIB CHOWDHURY <152077471+SYSTEM-ERROR-KING@users.noreply.github.com>
2024-05-28 18:42:17 +0600

    Update Rankup.js

--- scripts/commands/Rankup.js
+++ scripts/commands/Rankup.js
@@ -1,7 +1,7 @@
 module.exports.config = {
     name: "rankup",
     version: "1.0.0",
-    permission: 2,
+    permission: 0,
     credits: "farhan",
     description: "Restart Bot",
     prefix: true, 
