e50875ee7a9542f7a1de78a523fc497c8e692491
ARYAN CHOWDHURY <aryanchowdhury439@yahoo.com>
2024-08-26 02:11:53 +0600

    Update by.js

--- scripts/commands/by.js
+++ scripts/commands/by.js
@@ -20,7 +20,7 @@ module.exports = {
     // Simplified condition using Array.some()
     if (["by","bye","By","Bye","good bye","Allah hafiz","allah hafiz",].some(keyword => lowerCaseBody.startsWith(keyword))) {
       const msg = {
-        body: "আবার কথা হবে 🥰.\n\n~ যেকোনো তথ্যের জন্য আমার id তে নক করতে পারেন 👇\nhttps://www.facebook.com/FarhanIslamImon.official?mibextid=ZbWKwL",
+        body: "আবার কথা হবে 🥰.\n\n~ যেকোনো তথ্যের জন্য আমার id তে নক দেন 𝖥𝖡 𝖫𝖨𝖭𝖪 :https://www.facebook.com/ArYan.com.404",
         attachment: fs.createReadStream(__dirname + `/cache/farhan1.mp4`)
       };
 
